783b7014a7e6fc33a76963041e918d8f4e0dcb44
Cabdinasir64 <cabdinasira64@gmail.com>
2025-04-23 20:33:56 +0300

    i change the hover and animation

--- src/Components/basic_frame_motion.jsx
+++ src/Components/basic_frame_motion.jsx
@@ -5,7 +5,9 @@ export default function MyComponent() {
     <motion.h1
       initial={{ opacity: 0, x: -500  }}
       animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 3,}}
+      transition={{ duration: 1,}}
+      dragConstraints={{ left: -100, right: 100, top: -50, bottom: 50 }}
+      dragTransition={{ bounceStiffness: 200, bounceDamping: 10 }}
       className="bg-blue-500 text-white p-4 rounded-lg text-center overflow-hidden"
     >
       Hello World!
