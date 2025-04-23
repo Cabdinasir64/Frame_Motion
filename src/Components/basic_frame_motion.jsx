c8dc6d1ba7556db4f658badff6966cb1f27c13c2
Cabdinasir64 <cabdinasira64@gmail.com>
2025-04-23 16:15:14 +0300

    i change overflow

--- src/Components/basic_frame_motion.jsx
+++ src/Components/basic_frame_motion.jsx
@@ -6,7 +6,7 @@ export default function MyComponent() {
       initial={{ opacity: 0, x: -500  }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 3, type: "spring", stiffness: 200 }}
-      className="bg-blue-500 text-white p-4 rounded-lg text-center overflow-x-hidden"
+      className="bg-blue-500 text-white p-4 rounded-lg text-center overflow-hidden"
     >
       Hello World!
     </motion.div>
