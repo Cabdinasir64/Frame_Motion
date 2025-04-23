269bace60f172fe8d71c0fce4e4979a3d9bc650f
Cabdinasir64 <cabdinasira64@gmail.com>
2025-04-23 16:16:17 +0300

    i change the way the coming

--- src/Components/basic_frame_motion.jsx
+++ src/Components/basic_frame_motion.jsx
@@ -2,13 +2,13 @@ import { motion } from "framer-motion";
 
 export default function MyComponent() {
   return (
-    <motion.div
+    <motion.h1
       initial={{ opacity: 0, x: -500  }}
       animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 3, type: "spring", stiffness: 200 }}
+      transition={{ duration: 3,}}
       className="bg-blue-500 text-white p-4 rounded-lg text-center overflow-hidden"
     >
       Hello World!
-    </motion.div>
+    </motion.h1>
   );
 }
