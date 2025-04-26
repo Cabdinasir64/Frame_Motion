52ec10485ddfc96aea1bf4b4e61dd0ce85344638
Cabdinasir64 <cabdinasira64@gmail.com>
2025-04-26 16:46:00 +0300

    i added file of animation5 frame motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import Basic_Frame_Motion from './Components/basic_frame_motion'
-import Navbar from './Components/Navbar'
-import Dropdown from './Components/dropdown'
-import Animation1 from './Components/animation1'
-import Animation2 from './Components/animation2'
-import Animation3 from './Components/animation3'
-import Animation4 from './Components/animation4'
+import Animation5 from './Components/animation5'
 function App() {
 
   return (
     <>
-      <Animation4 />
+      <Animation5 />
 
     </>
   )
---
+++ src/Components/animation5.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const Animation5 = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <motion.div
+                initial={{
+                    rotate: 0,
+                    borderRadius: "0%",
+                    scale: 0,
+                }}
+                animate={{
+                    rotate: [0, 180, 360],
+                    borderRadius: ["0%", "20%", "38%", "50%"],
+                    scale: [0, 1, 2, 1],
+                }}
+                transition={{
+                    type: "tween", 
+                    duration: 3,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                }}
+                className="w-24 h-24 bg-blue-400"
+            />
+        </div>
+    );
+};
+
+export default Animation5;
